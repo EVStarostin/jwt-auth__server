e4c5513cf5ecb0ce0a8fb3f1aef64c373d66d136
Evgenii Starostin <e.starostin89@gmail.com>
2018-12-06 15:44:12 +0300

    initial commit

---
+++ routes/api.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the API'
+  });
+});
+
+router.post('/posts', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secret_key', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post created...',
+        authData
+      });
+    }
+  });
+});
+
+router.post('/login', (req, res) => {
+  // Mock user
+  const user = {
+    id: 1,
+    userName: 'User',
+    email: 'user@gmail.ru'
+  }
+
+  jwt.sign({ user }, 'secret_key', { expiresIn: '30s' }, (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
+
+module.exports = router;
